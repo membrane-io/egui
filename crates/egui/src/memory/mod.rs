ab5c6ef53f6e7a2ec1c08e19e4193064e7e33b68
Juan Campa <juancampa@gmail.com>
2024-02-29 15:00:25 -0500

    Allow mutable access to Area state.

--- crates/egui/src/memory/mod.rs
+++ crates/egui/src/memory/mod.rs
@@ -1245,6 +1245,10 @@ impl Areas {
         self.areas.get(&id)
     }
 
+    pub fn get_mut(&mut self, id: Id) -> Option<&mut area::State> {
+        self.areas.get_mut(&id)
+    }
+
     /// All layers back-to-front, top is last.
     pub(crate) fn order(&self) -> &[LayerId] {
         &self.order
