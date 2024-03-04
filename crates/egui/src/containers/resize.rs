dc5defd04d9b17d6ac526056bf9d44940c21fe12
Juan Campa <juancampa@gmail.com>
2024-03-04 14:18:03 -0500

    Allow Resize to snap to a grid

--- crates/egui/src/containers/resize.rs
+++ crates/egui/src/containers/resize.rs
@@ -46,6 +46,8 @@ pub struct Resize {
 
     with_stroke: bool,
     drag_corner: Align2,
+
+    snap_to_size: Option<(Vec2, Vec2)>,
 }
 
 impl Default for Resize {
@@ -59,6 +61,7 @@ impl Default for Resize {
             default_size: vec2(320.0, 128.0), // TODO(emilk): preferred size of [`Resize`] area.
             with_stroke: true,
             drag_corner: Align2::RIGHT_BOTTOM,
+            snap_to_size: None,
         }
     }
 }
@@ -203,6 +206,12 @@ impl Resize {
         self
     }
 
+    #[inline]
+    pub fn snap_to_size(mut self, snap: Vec2, snap_offset: Vec2) -> Self {
+        self.snap_to_size = Some((snap, snap_offset));
+        self
+    }
+
     /// MEMBRANE: Sets the size of the [`Resize`] area from its id.
     pub fn request_size(ctx: &Context, id: Id, size: Vec2) {
         if let Some(mut state) = State::load(ctx, id) {
@@ -287,6 +296,12 @@ impl Resize {
 
         if let Some(user_requested_rect) = user_requested_rect {
             state.desired_size = user_requested_rect.size();
+
+            // MEMBRANE: Snap to dashboard grid
+            if let Some((snap, snap_offset)) = self.snap_to_size {
+                state.desired_size =
+                    ((state.desired_size) / snap).round() * snap - snap_offset * 2.0;
+            }
         } else {
             // We are not being actively resized, so auto-expand to include size of last frame.
             // This prevents auto-shrinking if the contents contain width-filling widgets (separators etc)
