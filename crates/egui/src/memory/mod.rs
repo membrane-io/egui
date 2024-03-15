cdb9933a0ef6e8e7b49149e958d24c0e9bcbbc83
Juan Campa <juancampa@gmail.com>
2024-03-15 17:27:39 -0400

    Allow forgetting of areas by id

--- crates/egui/src/memory/mod.rs
+++ crates/egui/src/memory/mod.rs
@@ -1245,6 +1245,10 @@ impl Areas {
         self.areas.get(&id)
     }
 
+    pub fn remove(&mut self, id: Id) {
+        self.areas.remove(&id);
+    }
+
     pub fn get_mut(&mut self, id: Id) -> Option<&mut area::State> {
         self.areas.get_mut(&id)
     }
