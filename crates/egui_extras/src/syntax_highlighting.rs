62b3070e9731905fbc538ea5462f751e85544800
Juan Campa <juancampa@gmail.com>
2024-04-29 15:58:47 -0400

    Make TokenType public. For syntax highlighting

--- crates/egui_extras/src/syntax_highlighting.rs
+++ crates/egui_extras/src/syntax_highlighting.rs
@@ -128,7 +128,7 @@ fn monospace_font_size(style: &egui::Style) -> f32 {
 #[cfg(not(feature = "syntect"))]
 #[derive(Clone, Copy, PartialEq, enum_map::Enum)]
 #[cfg_attr(feature = "serde", derive(serde::Deserialize, serde::Serialize))]
-enum TokenType {
+pub enum TokenType {
     Comment,
     Keyword,
     Literal,
@@ -218,7 +218,7 @@ pub struct CodeTheme {
     font_id: egui::FontId,
 
     #[cfg(not(feature = "syntect"))]
-    formats: enum_map::EnumMap<TokenType, egui::TextFormat>,
+    pub formats: enum_map::EnumMap<TokenType, egui::TextFormat>,
 }
 
 impl Default for CodeTheme {
