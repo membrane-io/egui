81280524a40023e47e873975fa080f961a6c8840
Juan Campa <juancampa@gmail.com>
2024-09-03 14:31:15 -0400

    Tweak of pixel-alignment/gamma of glyphs

--- crates/epaint/src/text/fonts.rs
+++ crates/epaint/src/text/fonts.rs
@@ -863,6 +863,7 @@ impl FontImplCache {
                     ab_glyph_font,
                     scale_in_pixels,
                     tweak,
+                    scale_in_points,
                 ))
             })
             .clone()
