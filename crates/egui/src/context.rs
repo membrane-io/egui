a93a2ded4e73f3f4bae1d8f1c7bf8facb72898a9
Juan Campa <juancampa@gmail.com>
2025-02-04 23:12:38 -0500

    No clipping for debug painter

--- crates/egui/src/context.rs
+++ crates/egui/src/context.rs
@@ -1532,7 +1532,9 @@ impl Context {
 
     /// Paint on top of _everything_ else (even on top of tooltips and popups).
     pub fn debug_painter(&self) -> Painter {
-        Self::layer_painter(self, LayerId::debug())
+        // MEMBRANE: no clipping for debug painter, important when layer is transformed
+        let screen_rect = Rect::EVERYTHING;
+        Painter::new(self.clone(), LayerId::debug(), screen_rect)
     }
 
     /// Print this text next to the cursor at the end of the pass.
