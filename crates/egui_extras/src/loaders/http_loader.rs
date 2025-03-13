9391b07c2ee81c4033776c25622fd5df0c3002a1
Juan Campa <juancampa@gmail.com>
2025-03-13 12:40:28 -0400

    Suppress warnings

--- crates/egui_extras/src/loaders/http_loader.rs
+++ crates/egui_extras/src/loaders/http_loader.rs
@@ -61,11 +61,12 @@ impl EhttpLoader {
     }
 }
 
-const PROTOCOLS: &[&str] = &["http://", "https://"];
+// MEMBRANE: commenting out unused code to suppress warnings
+// const PROTOCOLS: &[&str] = &["http://", "https://"];
 
-fn starts_with_one_of(s: &str, prefixes: &[&str]) -> bool {
-    prefixes.iter().any(|prefix| s.starts_with(prefix))
-}
+// fn starts_with_one_of(s: &str, prefixes: &[&str]) -> bool {
+//     prefixes.iter().any(|prefix| s.starts_with(prefix))
+// }
 
 impl BytesLoader for EhttpLoader {
     fn id(&self) -> &str {
