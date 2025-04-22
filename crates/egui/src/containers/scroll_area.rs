7744ad3040008d3ec093f0cc7283f3238ccaf8da
Romet Tagobert <romet@tagobert.me>
2025-04-22 21:49:27 +0300

    track scroll focus to improve behaviour with nested scrollables

--- crates/egui/src/containers/scroll_area.rs
+++ crates/egui/src/containers/scroll_area.rs
@@ -952,7 +952,11 @@ impl Prepared {
 
         let max_offset = content_size - inner_rect.size();
         let is_hovering_outer_rect = ui.rect_contains_pointer(outer_rect);
-        if scrolling_enabled && is_hovering_outer_rect {
+
+        let scroll_focus = ui.ctx().input_mut(|input| input.scroll_focus);
+        let someone_else_scrolling = scroll_focus.is_some_and(|(other_id, _)| other_id != id);
+
+        if scrolling_enabled && is_hovering_outer_rect && !someone_else_scrolling {
             let always_scroll_enabled_direction = ui.style().always_scroll_the_only_direction
                 && scroll_enabled[0] != scroll_enabled[1];
             for d in 0..2 {
@@ -974,6 +978,8 @@ impl Prepared {
 
                         // Clear scroll delta so no parent scroll will use it:
                         ui.ctx().input_mut(|input| {
+                            input.scroll_focus = Some((id, content_is_too_large));
+
                             if always_scroll_enabled_direction {
                                 input.smooth_scroll_delta[0] = 0.0;
                                 input.smooth_scroll_delta[1] = 0.0;
