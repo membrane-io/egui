5bfe8a0f50ee2b16fbb38605a645b2ba8c94db2d
Thomas Seeley <seeleypersonal@gmail.com>
2025-09-23 07:36:13 -0400

    change bg_corner_radius to u8

--- crates/epaint/src/text/text_layout_types.rs
+++ crates/epaint/src/text/text_layout_types.rs
@@ -405,7 +405,7 @@ pub struct TextFormat {
 
     /// Corner radius for background rectangles.
     /// Default: 0.0 (no rounding)
-    pub bg_corner_radius: f32,
+    pub bg_corner_radius: u8,
 
     pub italics: bool,
 
@@ -436,7 +436,7 @@ impl Default for TextFormat {
             background: Color32::TRANSPARENT,
             expand_bg: Vec2::new(1.0, 1.0),
             coords: VariationCoords::default(),
-            bg_corner_radius: 0.0,
+            bg_corner_radius: 0,
             italics: false,
             underline: Stroke::NONE,
             strikethrough: Stroke::NONE,
@@ -472,7 +472,7 @@ impl std::hash::Hash for TextFormat {
         emath::OrderedFloat(expand_bg.x).hash(state);
         emath::OrderedFloat(expand_bg.y).hash(state);
         coords.hash(state);
-        emath::OrderedFloat(*bg_corner_radius).hash(state);
+        bg_corner_radius.hash(state);
         italics.hash(state);
         underline.hash(state);
         strikethrough.hash(state);
